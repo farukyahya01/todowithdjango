95e257d6cefb05a1fa1b1e57273bd529107d0022
farukyahya01 <ifarukaksu@gmail.com>
2022-06-22 13:33:28 +0300

    *connection postgreSQL

--- todowithdjango/settings.py
+++ todowithdjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todowithdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todowithdjango',
+        'USER': 'postgres',
+        'PASSWORD': 'testkullanıcı1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 

829ba3486c352ead34ce43f5eeb5884b67dcd9cd
farukyahya01 <ifarukaksu@gmail.com>
2022-06-23 14:32:08 +0300

    *APIView to GenericAPIView

--- todo/api/views.py
+++ todo/api/views.py
@@ -1,55 +1,84 @@
 from django.shortcuts import render, redirect
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
 from todo.models import TodoList
 from django.contrib.auth.models import User
 from todo.api.serializers import TodoListSerializers, UserSerializers
 
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+
+
+# GENERICS API VIEW
+class TodoListView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializers
+
+    def get(self, request , *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class TodoListDetailView(GenericAPIView):
+    pass
+
+class UserListView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+
+    def get(self, request , *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
 ### CLASS BASED ###
-class TodoListView(APIView):
-    def get(self, request):
-        todolist = TodoList.objects.all()
-        serializer = TodoListSerializers(todolist, many=True)
-        return Response(serializer.data)
+# class TodoListView(APIView):
+#     def get(self, request):
+#         todolist = TodoList.objects.all()
+#         serializer = TodoListSerializers(todolist, many=True)
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = TodoListSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = TodoListSerializers(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-class TodoListDetailView(APIView):
-    def get_object(self, pk):
-        todolist_detail = get_object_or_404(TodoList, pk=pk)
-        return todolist_detail
+# class TodoListDetailView(APIView):
+#     def get_object(self, pk):
+#         todolist_detail = get_object_or_404(TodoList, pk=pk)
+#         return todolist_detail
 
-    def get(self, request, pk):
-        todo = self.get_object(pk=pk)
-        serializer = TodoListSerializers(todo)
-        return Response(serializer.data)
+#     def get(self, request, pk):
+#         todo = self.get_object(pk=pk)
+#         serializer = TodoListSerializers(todo)
+#         return Response(serializer.data)
 
-    def put(self, request, pk):
-        todo = self.get_object(pk=pk)
-        serializer = TodoListSerializers(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         todo = self.get_object(pk=pk)
+#         serializer = TodoListSerializers(todo, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    def delete(self, request, pk):
-        todo = self.get_object(pk=pk)
-        todo.delete()
-        return Response(status= status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         todo = self.get_object(pk=pk)
+#         todo.delete()
+#         return Response(status= status.HTTP_204_NO_CONTENT)
 
-class UserListView(APIView):
-    def get(self ,request):
-        userlist = User.objects.all()
-        serializer = UserSerializers(userlist, many=True, context={'request': request})
-        return Response(serializer.data)
+# class UserListView(APIView):
+#     def get(self ,request):
+#         userlist = User.objects.all()
+#         serializer = UserSerializers(userlist, many=True, context={'request': request})
+#         return Response(serializer.data)
 
 
 
